28a2cf9ba97fed8bd4ce5c26c9236ec022e0235f
dimava <dimava2@ya.ru>
2022-11-12 01:38:26 +0300

    add --vue --react --config arguments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fs from 'node:fs';
 import minimist from 'minimist';
 import {readPackageUpSync} from 'read-pkg-up';
 import {writePackageSync} from 'write-pkg';
-import execa from 'execa';
+import execa, {sync as execaSync} from 'execa';
 import hasYarn from 'has-yarn';
 
 const DEFAULT_TEST_SCRIPT = 'echo "Error: no test specified" && exit 1';
@@ -67,6 +67,14 @@ export default async function createXo(options = {}) {
 	const cli = minimist(options.args || process.argv.slice(2));
 	delete cli._;
 
+	const dependencies = packageJson.dependencies || {};
+	const isVue = Boolean(dependencies.vue) || Boolean(cli.vue);
+	const isReact = Boolean(dependencies.react) || Boolean(cli.react);
+	const makeConfigFile = Boolean(cli.config);
+	delete cli.vue;
+	delete cli.react;
+	delete cli.config;
+
 	for (const option of PLURAL_OPTIONS) {
 		if (cli[option]) {
 			cli[`${option}s`] = [cli[option]].flat();
@@ -87,16 +95,28 @@ export default async function createXo(options = {}) {
 		warnConfigFile(packageCwd);
 	};
 
+	if (makeConfigFile) {
+		writeConfigFile(path.join(packageCwd, '.xo-config.cjs'), {isVue, isReact});
+	}
+
 	if (options.skipInstall) {
 		post();
 		return;
 	}
 
+	const packages = [
+		'xo',
+		isVue && 'eslint-config-xo-vue',
+		isReact && 'eslint-config-xo-react',
+		makeConfigFile && 'eslint-define-config',
+	].filter(Boolean);
+
 	if (hasYarn(packageCwd)) {
 		try {
-			const version = Number.parseFloat(await execa('yarn', ['--version'], {cwd: packageCwd}));
-			const arguments_ = version > 1 ? ['add', '--dev', 'xo'] : ['add', '--dev', '--ignore-workspace-root-check', 'xo'];
-			await execa('yarn', arguments_, {cwd: packageCwd});
+			const yarnVersion = execaSync('yarn', ['--version'], {cwd: packageCwd});
+			const version = Number.parseFloat(yarnVersion.stdout);
+			const arguments_ = version > 1 ? ['add', '--prefer-dev', ...packages] : ['add', '--dev', '--ignore-workspace-root-check', ...packages];
+			execaSync('yarn', arguments_, {cwd: packageCwd, stdio: 'inherit'});
 			post();
 		} catch (error) {
 			if (error.code === 'ENOENT') {
@@ -110,6 +130,27 @@ export default async function createXo(options = {}) {
 		return;
 	}
 
-	await execa('npm', ['install', '--save-dev', 'xo'], {cwd: packageCwd});
+	await execa('npm', ['install', '--save-dev', ...packages], {cwd: packageCwd});
 	post();
+}
+
+function writeConfigFile(path, {isVue, isReact}) {
+	const configFile = `
+const { defineConfig } = require('eslint-define-config');
+module.exports = defineConfig({
+	// root: true,
+
+	rules: {
+		// intellisence supported
+	},
+
+	extends: [
+		${['xo', isVue && 'xo-vue', isReact && 'xo-react']
+		.filter(Boolean)
+		.map(x => `'${x}',`).join('\n\t\t')
+}
+	],
+
+});`;
+	fs.writeFileSync(path, configFile);
 }
